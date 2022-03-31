e1873fbfe11c1eb2c74dd6b10586ea2f9bea4c78
mbence01 <h048514@stud.u-szeged.hu>
2022-04-01 01:28:33 +0200

    add dbconnector class

---
+++ DBConnector.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once("EnvReader.php");
+
+class DBConnector {
+    private $conn = false;
+
+    public function __construct($fname = null) {
+        $env = null;
+
+        if($env == null)
+            $env = new EnvReader();
+        else
+            $env = new EnvReader($fname);
+
+        $this->conn = oci_connect($env->readValue("DB_USER"), $env->readValue("DB_PASS"), $env->readValue("DB_TNS"));
+    }
+
+    public function __destruct() {
+        if($this->conn) {
+            oci_close($this->conn);
+            $this->conn = false;
+        }
+        return 0;
+    }
+
+    public function getConn() {
+        return $this->conn;
+    }
+
+    public function closeConn() {
+        return $this->__destruct();
+    }
+}
+
+?>
