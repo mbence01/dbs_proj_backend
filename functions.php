c14d43face8b3955c36a6ecdefa64f17eff9e937
mbence01 <h048514@stud.u-szeged.hu>
2022-04-01 11:47:28 +0200

    created functions file, add showAlert()

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+
+const ALERT_TYPE_HISTORY_BACK = 0;
+const ALERT_TYPE_STAY_HERE = 1;
+
+function showAlert($message, $path) {
+    $location = "'" . $path . "'";
+
+    if($path == ALERT_TYPE_HISTORY_BACK)
+        $location = "history.back()";
+    if($path == ALERT_TYPE_STAY_HERE)
+        $location = "window.location.pathname";
+
+    echo "
+            <script type='text/javascript'>
+                window.alert('" . $message . "');
+                window.location = " . $location . ";
+            </script>
+         ";
+}
+
+?>
