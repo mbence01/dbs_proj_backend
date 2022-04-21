d9c51b80963ee529c84d4d96374a8ef82db1dc47
mbence01 <h048514@stud.u-szeged.hu>
2022-04-21 15:42:02 +0200

    add userlogin save to login script

--- login.php
+++ login.php
@@ -27,6 +27,7 @@ foreach($required_fields as $key => $value) {
 
 
 require_once("db/models/Account.php");
+require_once("db/models/UserLogin.php");
 
 $loginCreds  = array(
     "u_username" => array($_POST["u_username"],      Account::REL_EQUALS),
@@ -35,6 +36,16 @@ $loginCreds  = array(
 
 if(!Account::existsWith($loginCreds))
     showAlert("Hibás felhasználónév vagy jelszó!", ALERT_TYPE_HISTORY_BACK);
+
+$userObj = Account::findAll($loginCreds)[0];
+$_SESSION["userData"] = serialize($userObj);
+
+// Save log of user login
+$userLogin = new UserLogin();
+$userLogin->setUId($userObj->getUId());
+$userLogin->setUlIpaddr($_POST["REMOTE_ADDR"]);
+$userLogin->setUlAgent($_POST["HTTP_USER_AGENT"]);
+UserLogin::addNew($userLogin);
 
 showAlert("Sikeres bejelentkezés!", "index.php");
 
