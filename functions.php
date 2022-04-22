9706d4f8c53c9a407a8389a331e73c42a475858e
mbence01 <h048514@stud.u-szeged.hu>
2022-04-22 21:28:24 +0200

    add defines

--- functions.php
+++ functions.php
@@ -3,6 +3,10 @@
 const ALERT_TYPE_HISTORY_BACK = 0;
 const ALERT_TYPE_STAY_HERE = 1;
 
+const PROFILE_DIRECTORY     = "profiles/"; // TODO: Change path
+const VIDEO_DIRECTORY       = "videostorage/"; // TODO: Change path
+const THUMBNAIL_DIRECTORY   = "videostorage/thumbnails/"; // TODO: Change path
+
 function showAlert($message, $path) {
     $location = "'" . $path . "'";
 
@@ -17,6 +21,7 @@ function showAlert($message, $path) {
                 window.location = " . $location . ";
             </script>
          ";
+    return 0;
 }
 
 function isUserLogged(): bool {
